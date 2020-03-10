720f865a5e15d0cc9dca366272c3217a84a6022a
Rufin Hounkpe <rhounkpe@gmail.com>
2020-03-10 13:33:20 +0100

    RestController

---
+++ src/main/java/com/pluralsight/conference/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conference.controller;
+
+import com.pluralsight.conference.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    public User getUser(@RequestParam(value = "firstName", defaultValue = "Rufin") String firstName,
+                        @RequestParam(value = "lastName", defaultValue = "Hounkpe") String lastName,
+                        @RequestParam(value = "age", defaultValue = "43") int age) {
+        User user = new User();
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAge(age);
+
+        return user;
+    }
+}
