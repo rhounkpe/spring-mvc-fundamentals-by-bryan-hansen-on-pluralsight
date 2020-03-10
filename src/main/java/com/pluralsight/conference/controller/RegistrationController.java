29ffb191855f99d104ebcffa6b38f64c1bc91d11
Rufin Hounkpe <rhounkpe@gmail.com>
2020-03-10 10:42:48 +0100

    Resolving a view

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Map;
-
 @Controller
 public class RegistrationController {
     @GetMapping("registration")
